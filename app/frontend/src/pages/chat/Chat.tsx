f41b18b9f94fb9c319a5217870b3987f7a103645
aratbe <63820764+aratbe@users.noreply.github.com>
2024-06-20 09:00:25 -0400

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -340,8 +340,8 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            <h1 className={styles.chatEmptyStateTitle}>Ask me about cybersecurity and cyber regulation</h1>
+                            <h4 className={styles.chatEmptyStateSubtitle}>I know NIST 800-123/82, 800-82, EBM hardening, EU CRA, Colorado AI Act, and some SE products </h4>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
                         </div>
                     ) : (
