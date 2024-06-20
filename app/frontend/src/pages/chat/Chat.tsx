9e268ac479cdccfba36136918f7ff90eb5e0e31d
aratbe <63820764+aratbe@users.noreply.github.com>
2024-06-20 07:14:45 -0400

    Update Chat.tsx for cyber example

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -413,7 +413,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. what are the security topics covered in the NIST hardening standard?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                             showSpeechInput={showSpeechInput}
