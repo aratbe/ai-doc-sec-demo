3d4ed78a088dde5ea62aaa74b59cd934ef3123b2
aratbe <63820764+aratbe@users.noreply.github.com>
2024-06-20 07:05:57 -0400

    Cyber context Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -3,15 +3,17 @@ import { Example } from "./Example";
 import styles from "./Example.module.css";
 
 const DEFAULT_EXAMPLES: string[] = [
-    "What is included in my Northwind Health Plus plan that is not in standard?",
-    "What happens in a performance review?",
-    "What does a Product Manager do?"
+    "What does BMS stand for?",
+    "Should a BMS network be connected to the internet?",
+    "If a user is required to perform maintenance of the system from a remote location, how should the user connect to the system without making the system vulnerable and exploitable?",
+    "What are the best practices to follow while setting up a BACnet/IP controller?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
-    "Compare the impact of interest rates and GDP in financial markets.",
-    "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
-    "Can you identify any correlation between oil prices and stock market trends?"
+    "Compare the topics that are in the EcoStruxure Building Management System Hardening Guide with the NIST 800-123 hardening guidelines",
+    "Can a Webstation in an EBO be exposed to the internet?",
+    "Consider the defense-in-depth security architecture for a DCS system suggested in the NIST. Can you compare it with Schneider Electric Machine Solutions architecture?"
+    
 ];
 
 interface Props {
